fb6f0792a29f77db88ce819b45f97ae3600a9eb6
kit <abhayasingh@yahoo.com>
2015-02-24 18:00:13 -0500

    finished ep2 of tutorial

---
+++ src/com/youtube/rest/status/V1_status.java
@@ -0,0 +1,39 @@
+package com.youtube.rest.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_status {
+
+	private static final String apiVersion = "00.01.00";
+	
+	/**
+	* This method sits at the root of the api. It will return the name
+	* of this api.
+	*
+	* @return String - Title of the api
+	*/
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle() {
+	return "<p>Java Web Service</p>";
+	}
+	
+	/**
+	* This method will return the version number of the api
+	* Note: this is nested one down from the root. You will need to add version
+	* into the URL path.
+	*
+	* Example:
+	* http://localhost:7001/com.youtube.rest/api/v1/status/version
+	*
+	* @return String - version number of the api
+	*/
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion() {
+	return "<p>Version:</p>" + apiVersion;
+	}
+}
